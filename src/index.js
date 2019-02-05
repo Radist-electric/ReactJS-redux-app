8bd679edd5b087bd30990187b98c51d4e1b5b7f5
Radist-electric <radist-electric@yandex.ru>
2019-02-05 12:36:58 +0500

    Lesson-13 only Redux

--- src/index.js
+++ src/index.js
@@ -1,40 +1,18 @@
-import {createStore} from 'redux';
-
-const reducer = (state = 0, action) => {
-    switch (action.type) {
-        case 'INC':
-            return state + 1;
-        case 'DEC':
-            return state - 1;
-        case 'RND':
-            return state + action.value;
-        case 'RESET':
-            return state = action.value;
-        default:
-            return state;
-    }
-}
-
-const inc = () => ({type: 'INC'});
-const dec = () => ({type: 'DEC'});
-// const rnd = (value) => ({type: 'RND', value});
-const reset = (value) => ({type: 'RESET', value});
+import {createStore, bindActionCreators} from 'redux';
+import reducer from './reducer';
+import * as actions from './actions';
 
 const store = createStore(reducer);
+const {dispatch} = store;
 
-document.getElementById('inc').addEventListener('click', () => {
-    store.dispatch(inc())
-});
-document.getElementById('dec').addEventListener('click', () => {
-    store.dispatch(dec())
-});
-// document.getElementById('rnd').addEventListener('click', () => {
-//     const value = Math.floor(Math.random() * 10 + 1);
-//     store.dispatch(rnd(value))
-// });
+const {inc, dec, reset} = bindActionCreators(actions, dispatch);
+
+document.getElementById('inc').addEventListener('click', inc);
+document.getElementById('dec').addEventListener('click', dec);
 document.getElementById('reset').addEventListener('click', () => {
-    store.dispatch(reset(0))
+    reset(0)
 });
+
 const update = () => {
     document.getElementById('counter').textContent = store.getState();
 }
